4ae83c38cd56160fbf19739daae3ce1a4729d138
SUKUMAR <SUKUMARREDDY4498@GMAIL.COM>
2022-04-10 00:19:09 -0500

    Initial Commit

---
+++ video_app/video_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  video_appApp.swift
+//  video_app
+//
+//  Created by apple on 10/04/22.
+//
+
+import SwiftUI
+
+@main
+struct video_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
